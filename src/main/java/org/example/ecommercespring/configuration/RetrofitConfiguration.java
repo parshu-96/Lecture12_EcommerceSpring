93723b9aa4e7502ace81d64224b51c44b72672db
parshu <parshuvmahindrakar@gmail.com>
2025-06-28 22:17:24 +0530

    HW 1:Updated code to store base url of retrofit in .env file

--- src/main/java/org/example/ecommercespring/configuration/RetrofitConfiguration.java
+++ src/main/java/org/example/ecommercespring/configuration/RetrofitConfiguration.java
@@ -1,6 +1,7 @@
 package org.example.ecommercespring.configuration;
 
 import org.example.ecommercespring.gateway.api.IFakeStoreCategoryAPI;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import retrofit2.Retrofit;
@@ -8,10 +9,13 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 @Configuration
 public class RetrofitConfiguration {
+
+    @Value("${FAKE_STORE_BASE_URL}") // Name should match key in .env
+    private String baseUrl;
     @Bean
     public Retrofit retrofit(){
         return new Retrofit.Builder()
-                .baseUrl("https://fakestoreapi.in/api/")
+                .baseUrl(baseUrl)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
     }
